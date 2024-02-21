db2f5aa75d6bb1fe5acb538c54cc393864811bdd
dom <dompriem22@gmail.com>
2024-02-21 10:45:45 +0200

    files updated with code

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// counter funtionality
 
+const  decrease = document.getElementById("decrease");
+const  reset = document.getElementById("reset");
+const  increase = document.getElementById("increase");
+const  countDisplay = document.getElementById("count-display");
+let count = 0;
+
+
+increase.onclick = function(){
+  count++;
+  countDisplay.textContent = count;
+}
+
+reset.onclick = function(){
+  count=0;
+  countDisplay.textContent = count;
+}
+
+decrease.onclick = function(){
+  count--;
+  countDisplay.textContent = count;
+}
